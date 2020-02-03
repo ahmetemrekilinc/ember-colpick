2a1cfe5ea1bd934a09753323cfeeb2c0d19c4a7e
esrgrlk <esragurluk@gmail.com>
2020-02-03 11:41:01 +0300

    ember-cli is upgraded to 3.15.0

--- app/components/col-pick-input.js
+++ app/components/col-pick-input.js
@@ -1,5 +1,5 @@
 import TextField from '@ember/component/text-field';
-import ColPickMixin from 'ember-colpick/mixins/col-pick';
+import ColPickMixin from 'ember-colpick-included-fixed/mixins/col-pick';
 
 export default TextField.extend(ColPickMixin, {
   flat: false
